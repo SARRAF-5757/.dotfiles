89f3848875c77dd4c9547dd660a1878d1b4c2d4e
SARRAF <61262059+SARRAF-5757@users.noreply.github.com>
2025-04-20 23:08:53 -0400

    Center window script added + wallpaper adjustment

---
+++ Scripts/center-window.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Center Window
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ↔️
+
+# Documentation:
+# @raycast.description Almost-maximizes a window and centers it using Raycast Window Management
+# @raycast.author SARRAF
+# @raycast.authorURL https://raycast.com/SARRAF
+
+open -g raycast://extensions/raycast/window-management/maximize
+open -g raycast://extensions/raycast/window-management/make-smaller
+open -g raycast://extensions/raycast/window-management/center 
