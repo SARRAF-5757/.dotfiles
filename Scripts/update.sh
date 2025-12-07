3d4bd4262beffd644ceb5f7fdc5ec50539a79b35
SARRAF-5757 <61262059+SARRAF-5757@users.noreply.github.com>
2025-12-07 11:58:16 -0500

    update script changes

--- Scripts/update.sh
+++ Scripts/update.sh
@@ -13,19 +13,41 @@
 # @raycast.authorURL https://raycast.com/SARRAF
 
 # Color Variables
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
-RED='\033[1;31m'
 PURPLE='\033[1;35m'
-NC='\033[0m' # No Color
+CYAN='\033[1;36m'
+NC='\033[0m'
 
 print "${YELLOW}\ue7fd Updating Brew...${NC}"
-brew upgrade
-brew cleanup
+brew update && brew upgrade && brew cleanup
 print " "
 print " "
 print "${BLUE}\ue711 Updating App Store...${NC}"
 mas upgrade
+print " "
+print " "
+print "${BLUE}\uf179 Checking macOS Updates...${NC}"
+softwareupdate -l
+print " "
+print " "
+print "${GREEN}\ue71e Updating npm...${NC}"
+npm install -g npm@latest
+npm update -g
+print " "
+print " "
+print "${PURPLE}\uf1d3 Updating Oh My Zsh...${NC}"
+$ZSH/tools/upgrade.sh
+print " "
+print " "
+print "${PURPLE}\uf489 Updating OMP...${NC}"
+oh-my-posh upgrade
+print " "
+print " "
+print "${CYAN}\uf36f Updating Neovim Plugins...${NC}"
+nvim "+Lazy! sync" +qa
 print " "
 print " "
 print "${RED}\ueae6 Updating raycast extensions...${NC}"
@@ -36,8 +58,5 @@ print "${RED}\ueac9 Updating Raycast...${NC}"
 open -g raycast://extensions/raycast/raycast/check-for-updates
 print " "
 print " "
-print "${PURPLE}\uf489 Updating OMP...${NC}"
-oh-my-posh upgrade
 print "Update Finished" | figlet | lolcat
-print " "
-print " "
+
